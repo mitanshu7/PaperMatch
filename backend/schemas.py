8da6875e30a7f185e0f2a8426a46455bf50721ef
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2025-09-29 18:25:47 +0530

    update arxivpaper model for extra validation

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, HttpUrl, field_validator
+from pydantic import BaseModel, HttpUrl, field_validator, Field
 import re
+from datetime import datetime
+
 
 # Create a request model
 class TextRequest(BaseModel):
@@ -7,18 +9,19 @@ class TextRequest(BaseModel):
 
 
 arxiv_url_regex = re.compile(r".+arxiv\.org.+")
+current_year = datetime.now().year
 
 # Response model for arxiv papers
 class ArxivPaper(BaseModel):
-    id: str 
-    title: str
-    authors: list[str]
-    abstract: str
-    url: HttpUrl
-    pdf: HttpUrl
-    month: int
-    year: int
-    categories: list[str]
+    id: str = Field(min_length=1)
+    title: str = Field(min_length=1)
+    authors: list[str] = Field(min_length=1)
+    abstract: str = Field(min_length=1)
+    url: HttpUrl = Field(min_length=1)
+    pdf: HttpUrl = Field(min_length=1)
+    month: int = Field(ge=1, le=12)
+    year: int = Field(ge=1991, le=current_year)
+    categories: list[str] = Field(min_length=1)
     
     @field_validator("url", "pdf")
     @classmethod
